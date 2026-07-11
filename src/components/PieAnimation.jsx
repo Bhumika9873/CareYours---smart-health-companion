95d429e68c41cfeb1e2a7abb55a68045a1005bf7
Bhumika <bhumikaa9873@gmail.com>
2026-07-11 18:18:58 -0500

    Changing UI

--- src/components/PieAnimation.jsx
+++ src/components/PieAnimation.jsx
@@ -1,7 +1,7 @@
 // PART 12 : PieAnimation.jsx (Replace Complete File)
 
 import {useEffect,useState} from "react";
-import "./PieAnimation.css";
+import "./Dashboard.css";
 
 export default function PieAnimation(){
 const[target]=useState(82);
@@ -28,43 +28,24 @@ background:`conic-gradient(#7CB342 ${progress*3.6}deg,#E8F7DA 0deg)`
 >
 
 <div className="pie-inner">
-
 <h2>{progress}%</h2>
-
 <span>Excellent</span>
-
 </div>
-
 </div>
-
 <div className="pie-info">
-
 <div>
-
 <h4>Sleep</h4>
-
 <p>7.8 hrs</p>
-
 </div>
-
 <div>
-
 <h4>Water</h4>
-
 <p>2.4 L</p>
-
 </div>
-
 <div>
-
 <h4>Steps</h4>
-
 <p>8,524</p>
-
 </div>
-
 </div>
-
 </div>
 );
 }
